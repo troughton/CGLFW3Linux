e6ead33fe6ca007f01cf5017148b014c9247e018
Thomas Roughton <t.roughton@me.com>
2016-05-20 16:02:06 +1200

    Added access to native APIs

--- shim.h
+++ shim.h
@@ -3,3 +3,11 @@
 
 // find GLFW using include path
 #include "GLFW/glfw3.h"
+
+typedef void *GLXContext;
+
+#define GLX_H
+#define GLFW_EXPOSE_NATIVE_GLX
+#define GLFW_EXPOSE_NATIVE_X11
+#include "GLFW/glfw3native.h"
+#undef GLX_H
